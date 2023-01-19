4336273f326cb2b645b3cf2818401fd01e794971
ashishthomaschempolil <ashishthomas7@gmail.com>
2023-01-19 18:11:43 -0500

    Solved some of the bugs

--- app.js
+++ app.js
@@ -34,36 +34,58 @@ function changePreviousDisplay(arr){
 }
 
 function changeDisplay(value){
+    
     if (value === "="){
         //push the current display number to the previousResult and update the previosResult, currentResult
-        previousResult.append(currentDisplay.textContent)
-        currentResult = operate(operatorFunctions[previousResult[previousResult.length - 1]], currentResult, Number(value))
+        previousResult.push(currentDisplay.textContent)
+        currentResult = operate(
+            operatorFunctions[previousResult[previousResult.length - 2]],
+            currentResult,
+            Number(currentDisplay.textContent)
+        )
         changeCurrentDisplay(currentResult)
         changePreviousDisplay(previousResult)
+        previousResult = []
     }
     else if (value in operatorFunctions){
         // check for base case that is make sure the first item in previousResult is number and second is the operator
+        // if value in operatorFunctions then 
         if (previousResult[previousResult.length - 1] in operatorFunctions){
             previousResult.pop()
+            previousResult.push(value)
+            changePreviousDisplay(previousResult)
         }
-        else if(previousResult.length>2){
-            
-            currentResult = operate(
-                operatorFunctions[previousResult[previousResult.length-2]], 
-                currentResult, 
-                Number(previousResult[previousResult.length-1])
+        else{
+            number = currentDisplay.textContent
+            currentDisplay.textContent = ""
+            previousResult.push(number)
+            if (previousResult.length == 1){
+                currentResult = Number(number)
+            }
+            else if (previousResult.length > 2){
+                currentResult = operate(
+                    operatorFunctions[previousResult[previousResult.length - 2]],
+                    Number(number),
+                    currentResult
                 )
-            changeCurrentDisplay(currentResult);
-            previousResult.append(value);
-            changePreviousDisplay(previousResult);
+            }
+            previousResult.push(value)
+            //if the length of the previousResult.length>3 then calculate the result
+            changeCurrentDisplay(currentResult) // to show the result of the expression in currentDisplay
+            flag = 0 //flag variable to replace the number value with new inputted number
+            changePreviousDisplay(previousResult) // to show the expression in previousDisplay
         }
-        else {
-            currentResult = Number(currentDisplay.textContent)
-        }
+
     }
     else { //if its a number
         //update currentDisplay's textContent
-        currentDisplay.textContent += value
+        if (flag === 1){ 
+            currentDisplay.textContent += value
+        }
+        else{//overwrote the result of the expression with the new number
+            currentDisplay.textContent = value
+            flag = 1;
+        }
     }
 }
 
@@ -75,10 +97,10 @@ function clearDisplay(){
     currentResult = 0
     previousResult = []
     changeCurrentDisplay("")
-    changePreviousDisplay("")
+    changePreviousDisplay(previousResult)
 }
 
-let currentResult = 0, previousResult = [];
+let currentResult = 0, previousResult = [], flag = 0;
 
 
 const numbers = document.querySelectorAll(".number")
